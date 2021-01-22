2d06388a97bb04f5bfa88afd0b35d169541c60f7
Nikesh <nikesh.ramnani@gmail.com>
2021-01-22 21:51:12 +0530

    Pass list as props in class components

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class UserItem extends Component {
+
+  render() {
+    return (
+		<div className='card text-center'>
+			<img src={this.props.user.avatar_url} alt='' className='round-img' style={{width: '60px'}} />
+			<h3>{this.props.user.login}</h3>
+			<div>
+				<a href={this.props.user.html_url} className='btn btn-dark btn-sm my-1'>
+					More
+				</a>
+			</div>
+		</div>
+	);
+  }
+}
+
+export default UserItem;
