7cf41e5912ba89cff51644624da8154d443ae019
Nikesh <nikesh.ramnani@gmail.com>
2021-02-13 03:20:17 +0530

    Refactor the app to functional components using context api and hooks from class components

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,45 +1,40 @@
-import React, { Fragment, Component } from 'react';
+import React, { Fragment, useEffect, useContext } from 'react';
 import Spinner from '../layout/Spinner';
 import Repos from '../repos/Repos';
-import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import GithubContext from '../../context/github/githubContext';
 
-class User extends Component {
-	componentDidMount() {
-		this.props.getUser(this.props.match.params.login);
-		this.props.getUserRepos(this.props.match.params.login);
-	}
+const User = ({ match }) => {
+	const githubContext = useContext(GithubContext);
 
-	static propTypes = {
-		loading: PropTypes.bool,
-		user: PropTypes.object.isRequired,
-		repos: PropTypes.array.isRequired,
-		getUser: PropTypes.func.isRequired,
-		getUserRepos: PropTypes.func.isRequired,
-	}
+	const {getUser, loading, user,repos, getUserRepos} = githubContext;
+	
+	useEffect(() => {
+		getUser(match.params.login);
+		getUserRepos(match.params.login);
+		// eslint-disable-next-line
+	}, []);
 
-	render() {
-		const {
-			name,
-			company,
-			avatar_url,
-			location,
-			bio,
-			blog,
-			login,
-			html_url,
-			followers,
-			following,
-			public_repos,
-			public_gists,
-			hireable
-		} = this.props.user;
+	const {
+		name,
+		company,
+		avatar_url,
+		location,
+		bio,
+		blog,
+		login,
+		html_url,
+		followers,
+		following,
+		public_repos,
+		public_gists,
+		hireable
+	} = user;
 
-		const { loading, repos } = this.props;
+	if (loading) return <Spinner />;
 
-		if (loading) return <Spinner />;
-
-		return <Fragment>
+	return (
+		<Fragment>
 			<Link to='/' className='btn btn-light'>
 				Back to Search
 			</Link>
@@ -89,7 +84,7 @@ class User extends Component {
 			</div>
 			<Repos repos={repos} />
 		</Fragment>
-	}
+	);
 }
 
 export default User;
