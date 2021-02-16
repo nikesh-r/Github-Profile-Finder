bae3d6053be683c3bd54d8005ce43169f58d4ff0
Nikesh <nikesh.ramnani@gmail.com>
2021-02-16 21:51:36 +0530

    Small changes

--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -6,7 +6,7 @@ import {
 	GET_REPOS
 } from '../types';
 
-export default (state, action) => {
+const GithubReducer =  (state, action) => {
 	switch(action.type) {
 		case SEARCH_USERS:
 			return {
@@ -41,3 +41,5 @@ export default (state, action) => {
 			return state;
 	}
 }
+
+export default GithubReducer;
